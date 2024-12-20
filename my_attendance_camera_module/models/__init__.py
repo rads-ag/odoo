e663a8258082c040bed5019cb0fed45dfec5e5ca
rads-ag <92434454+rads-ag@users.noreply.github.com>
2024-12-20 18:29:04 +0530

    Delete my_attendance_camera_module directory

--- my_attendance_camera_module/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import hr_attendance
-from . import employee
-from . import config_settings
-from . import attendance_webcam
--- my_attendance_camera_module/models/hr_attendance.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class HrAttendance(models.Model):
-    _inherit = 'hr.attendance'
-
-    checkin_image = fields.Binary('Check-in Image')
-    checkout_image = fields.Binary('Check-out Image')
